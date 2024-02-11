bcca91a23e3e027567f40cb5f7e0614bff692e96
Aleks test <7501130l@gmail.com>
2024-02-11 22:28:49 +0300

    WorkFrelance

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+import org.example.work.services.WorkService;
+
+public class main {
+    public static void main(String[] args) {
+
+        WorkService service = new WorkService();
+
+        System.out.println(service.calculate(10_000,3_000,20_000));
+        System.out.println(service.calculate(100_000,60_000,150_000));
+
+
+    }
+}
---
+++ src/test/java/org/example/work/services/WorkServiceTest.java
@@ -0,0 +1,31 @@
+package org.example.work.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class WorkServiceTest {
+
+    @ParameterizedTest
+//    @CsvSource({
+ //           "3,10000,3000,20000",
+   //         "2,100000,60000,150000"
+    // })
+    @CsvFileSource(files = "src/test/resources/work.csv")
+    public void testWorkServiceMonth(int expected, int income, int expenses, int threshold) {
+
+        WorkService service = new WorkService();
+        int actual = (service.calculate(income, expenses, threshold));//      int expected = 3;
+        Assertions.assertEquals(expected,actual);
+    }
+ //   @Test
+ //   public void testWorkServiceTwоMonth() {
+ //      WorkService service = new WorkService();
+ //      int actual = (service.calculate(100_000, 60_000, 150_000));//      int expected = 2;
+ //       Assertions.assertEquals(expected,actual);
+    // }
+}
+
+
